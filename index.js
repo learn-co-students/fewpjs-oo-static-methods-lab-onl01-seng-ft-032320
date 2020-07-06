ca8a217fd29fde5a27548ba98db57df69fdaece1
antdp425 <antdp425@aol.com>
2020-07-06 19:51:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    string.charAt(0).toUpperCase()
+    return string = string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(sentence){
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    let split = sentence.split(" ");
+    let titleized = split.map(word => {
+      if (sentence.indexOf(word) == 0){
+        return this.capitalize(word) 
+      } else {
+        if (exceptions.includes(word)){
+          return word
+        } else {
+          return this.capitalize(word)
+        }
+      }
+    })
+    return titleized.join(" ")
+  }
 }
