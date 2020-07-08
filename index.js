4c7917de4a7477b4d99c7a5a810cae42019f9d99
ronniekram <arynn.boniface@gmail.com>
2020-07-08 15:26:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(string) {
+    let fillers = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    return string.split(' ').map(word => {
+      if (string.indexOf(word) == 0) {
+        return this.capitalize(word);
+      } else {
+        if (fillers.includes(word)) {
+          return word;
+        } else {
+          return this.capitalize(word);
+        }
+      }
+    }).join(' ');
+  
+  }
 }
