f94d0557ec59a8c71ac89050eef499bf5100b898
Beth Ruff <beth.ruff2@gmail.com>
2020-07-09 21:22:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  //keep dashes, single quotes, spaces
+
+  static titleize(str) {
+    let capitalizedStr = str.charAt(0).toUpperCase() + str.slice(1);
+    let lowerCaseWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let strArr = capitalizedStr.split(' ');
+    return strArr.map(str => {
+      if (lowerCaseWords.includes(str)) {
+        return str
+      } else {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+      }
+    }).join(' ');
+  }
 }
